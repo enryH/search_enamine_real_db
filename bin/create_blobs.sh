714f47d104e1874ece987e0cc2285ece2bc5961b
enryh <henry.webel@sund.ku.dk>
2019-10-29 18:49:58 +0100

    first try create blob shell script

---
+++ bin/create_blobs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+usage()
+{   
+    echo "Creates binary files for Enamine project."
+    echo "create_blobs -e rdkit-env --cpus 4 | [-h]]"
+    echo "-n: 2, 3, 4, 6 or 12"
+    echo "Example:"
+    # echo "qsub_executor.sh --script alignment.sh --numbers \"13 14 15 16\""
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --cpus )           shift
+                                cpus=$1
+                                ;;
+        -e | --env )            shift
+                                env=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+echo "Use $env and $cpus cpus"
+env=${env:-rdkit}
+cpus=${cpus:-2}
+#conda activate $env
+echo "Use $env and $cpus cpus"
+
+if ((12 % $cpus)); then
+    echo "Pleas select for --cups either 2, 3, 4, 6 or 12"
+    exit 1
+fi
+
+n_iter=$((12/$cpus))
+echo "Perform $n_iter loops."
+
+cmd=''
+for NO in {1..12..1}
+do
+    cmd="$cmd python create_blobs.py -n $NO &"
+    if ! (($NO % $cpus)); then
+        $cmd wait
+        cmd=''
+    fi
+done
